577f6a2484eef0d7ee5d1bdda08612916b9e5b12
kromych <kromych@users.noreply.github.com>
2023-07-06 00:10:18 -0700

    A test stub

--- aarch64/src/tests.rs
+++ aarch64/src/tests.rs
@@ -1,4 +1,22 @@
 #![cfg(test)]
 
+use crate::mmu::PageTableSpace;
+use crate::mmu::VirtualAddress;
+
 #[test]
-fn test_mmu() {}
+fn test_mmu() {
+    let mut space = vec![0; 0x10000];
+    let mut page_tables =
+        PageTableSpace::new(0x00000040248000, &mut space).expect("Can initialize page tables");
+
+    page_tables
+        .map_small_page(0x4000, VirtualAddress::from(0x4000))
+        .expect("can map a page");
+    page_tables
+        .map_small_page(0x5000, VirtualAddress::from(0x5000))
+        .expect("can map a page");
+
+    page_tables
+        .map_small_page(0x5000, VirtualAddress::from(0xffff_8000_0000_5000))
+        .expect("can map a page");
+}
