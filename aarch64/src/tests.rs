0e2eb322e85615c17fd27d837af7caf0369cd774
kromych <kromych@users.noreply.github.com>
2023-07-07 00:27:01 -0700

    More tests

--- aarch64/src/tests.rs
+++ aarch64/src/tests.rs
@@ -7,7 +7,7 @@ use crate::regs::MemoryAttributeEl1;
 use crate::regs::MemoryAttributeIndirectionEl1;
 
 #[test]
-fn test_mmu() {
+fn test_mmu_small_pages() {
     let mut space = vec![0xaa; 0x100000];
     let mut page_tables =
         PageTableSpace::new(0x00000040248000, &mut space).expect("Can initialize page tables");
@@ -63,4 +63,44 @@ fn test_mmu() {
     assert_eq!(res, Ok(()));
 
     std::fs::write("page_tables.bin", space).expect("can dump the page tables");
+}
+
+#[test]
+fn test_mmu_large_pages() {
+    let mut space = vec![0xaa; 0x100000];
+    let mut page_tables =
+        PageTableSpace::new(0x00000040248000, &mut space).expect("Can initialize page tables");
+
+    let mair_el1 = MemoryAttributeIndirectionEl1::default();
+    let wb_index = mair_el1
+        .get_index(MemoryAttributeEl1::Normal_WriteBack)
+        .expect("must be some WB memory available");
+
+    let res = page_tables.map_pages(
+        0,
+        VirtualAddress::from(0),
+        0x2000,
+        PageSize::Large,
+        wb_index,
+    );
+    assert_eq!(res, Ok(()));
+
+    std::fs::write("page_tables_large.bin", space).expect("can dump the page tables");
+}
+
+#[test]
+fn test_mmu_huge_pages() {
+    let mut space = vec![0xaa; 0x100000];
+    let mut page_tables =
+        PageTableSpace::new(0x00000040248000, &mut space).expect("Can initialize page tables");
+
+    let mair_el1 = MemoryAttributeIndirectionEl1::default();
+    let wb_index = mair_el1
+        .get_index(MemoryAttributeEl1::Normal_WriteBack)
+        .expect("must be some WB memory available");
+
+    let res = page_tables.map_pages(0, VirtualAddress::from(0), 4, PageSize::Huge, wb_index);
+    assert_eq!(res, Ok(()));
+
+    std::fs::write("page_tables_huge.bin", space).expect("can dump the page tables");
 }
